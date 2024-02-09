e6f4d57ffdc47271ec7b59fe496cc034b588f790
Grzeggorz <153080059+Grzeggorz@users.noreply.github.com>
2024-02-09 12:18:48 +0100

    Update losowe słowa generator.cpp

--- losowe słowa generator/losowe słowa generator.cpp
+++ losowe słowa generator/losowe słowa generator.cpp
@@ -2,10 +2,13 @@
 //
 
 #include <iostream>
+#include <cmath>
+using namespace std;
 
 int main()
 {
     std::cout << "Hello World!\n";
+    
 }
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
